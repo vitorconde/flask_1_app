055b8e360f73c9fb3bbd7d393f1e141a1919d46e
Vitor <vitor.condexd@gmail.com>
2019-12-26 15:55:58 -0300

    Aula 02
    Deixando tudo automatico, sem estar no código HTML.

--- jogoteca.py
+++ jogoteca.py
@@ -2,8 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Jogos():
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    jogo1 = Jogos('Super Mario', 'Ação', 'SNES')
+    jogo2 = Jogos('Pokemon Gold', 'RPG', 'GBA')
+    jogo3 = Jogos('Mortal Kombat', 'Luta', 'SNES')
+    lista=[jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
