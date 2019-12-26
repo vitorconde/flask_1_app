8cbb397c88aba7d915c7116318f11f077bba83bd
Vitor <vitor.condexd@gmail.com>
2019-12-26 17:00:03 -0300

    Aula 03
    Adicionando metodo de adicionar novo jogo na aplicação.

--- jogoteca.py
+++ jogoteca.py
@@ -1,19 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-class Jogos():
+class Jogo():
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogos('Super Mario', 'Ação', 'SNES')
-    jogo2 = Jogos('Pokemon Gold', 'RPG', 'GBA')
-    jogo3 = Jogos('Mortal Kombat', 'Luta', 'SNES')
-    lista=[jogo1, jogo2, jogo3]
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
+lista=[jogo1, jogo2, jogo3]
+
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+app.run(debug=True)
